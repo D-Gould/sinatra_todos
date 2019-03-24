58d3df27b4ef73df172340e71dedb74649edf14b
Dana Gould EOS <danagouldeos@Danas-MacBook-Pro-3.local>
2019-03-24 10:39:21 -0700

    Adding work through 'Creating a new todo list' assignment

--- todo.rb
+++ todo.rb
@@ -2,6 +2,29 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+before do
+  session[:lists] ||= []
+end
+
 get "/" do
-  erb "You have no lists.", layout: :layout
+  redirect "/lists"
+end
+
+get "/lists" do
+  @lists = session[:lists]
+  erb :lists
+end
+
+get "/lists/new" do
+  erb :new_list
+end
+
+post "/lists" do
+  session[:lists] <<{name: params[:list_name], todos: []}
+  redirect "/lists"
 end
