373fbc1bda6452e29c9c0ae200b99be9b8d09ca0
Dana Gould EOS <danagouldeos@Danas-MacBook-Pro-3.local>
2019-03-25 13:20:45 -0700

    Updating end of Saturday changes

--- todo.rb
+++ todo.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/content_for'
 require 'tilt/erubis'
 
 configure do
@@ -21,7 +22,20 @@ get '/lists' do
 end
 
 get '/lists/new' do
+  @lists = session[:lists]
   erb :new_list
+end
+
+get '/lists/:id' do
+  @list_id = params[:id].to_i
+  @list = session[:lists][@list_id]
+  erb :list
+end
+
+get '/lists/:id/edit' do
+  @list_id = params[:id].to_i
+  @list = session[:lists][@list_id]
+  erb :edit_list
 end
 
 # Returns error message if the list name causes an error. Else returns nil.
@@ -44,4 +58,27 @@ post '/lists' do
     session[:success] = 'The list has been created.'
     redirect '/lists'
   end
+end
+
+# Update an existing todo list
+post '/lists/:id' do
+  @list_id = params[:id].to_i
+  @list = session[:lists][@list_id]
+  list_name = params[:list_name].strip
+  error = error_message_finder(list_name)
+  if error
+    session[:error] = error
+    erb :edit_list
+  else
+    @list[:name] = list_name
+    session[:success] = 'The list has been updated.'
+    redirect "/lists/#{@list_id}"
+  end
+end
+
+post '/lists/:id/delete' do
+  @list_id = params[:id].to_i
+  session[:lists].delete_at(@list_id)
+  session[:success] = 'List successfully deleted.'
+  redirect '/lists'
 end
