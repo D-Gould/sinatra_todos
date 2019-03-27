bd951e7eac610ffa31f9b3ed1848fe08cb207fde
Dana Gould EOS <danagouldeos@Danas-MacBook-Pro-3.local>
2019-03-26 21:14:56 -0700

    final adds

--- Gemfile
+++ Gemfile
@@ -1,5 +1,10 @@
 source 'https://rubygems.org'
 
+ruby "2.4.4"
 gem 'erubis'
 gem 'sinatra', '~>1.4.7'
 gem 'sinatra-contrib'
+
+group :production do
+  gem "puma"
+end
--- todo.rb
+++ todo.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
-require 'sinatra/reloader'
+require 'sinatra/reloader' if development?
 require 'sinatra/content_for'
 require 'tilt/erubis'
 
@@ -10,6 +10,39 @@ end
 
 before do
   session[:lists] ||= []
+end
+
+#methods to be used in the views
+helpers do
+  def list_complete?(list)
+    list[:todos].all? {|todo| todo[:completed]} && list[:todos].size > 0
+  end
+
+  def todos_count(list)
+    list[:todos].count
+  end
+
+  def remaining_todos(list)
+    list[:todos].count {|todo| todo[:completed] == false}
+  end
+
+  def list_class(list)
+    "complete" if list_complete?(list)
+  end
+
+  def sort_list(lists, &block)
+    complete_lists, incomplete_lists = lists.partition { |list| list_complete?(list) }
+
+    incomplete_lists.each { |list| yield(list, lists.index(list)) }
+    complete_lists.each { |list| yield(list, lists.index(list)) }
+  end
+
+  def sort_todos(todos, &block)
+    complete_todos, incomplete_todos = todos.partition { |todo| todo[:completed] }
+
+    incomplete_todos.each { |todo| yield(todo, todos.index(todo)) }
+    complete_todos.each { |todo| yield(todo, todos.index(todo)) }
+  end
 end
 
 get '/' do
@@ -47,6 +80,13 @@ def error_message_finder(name)
   end
 end
 
+# Returns error message if the todo name causes an error. Else returns nil.
+def error_message_finder(name)
+  if !(1..100).cover? name.size
+    'Todo name must be between 1 and 200 characters.'
+  end
+end
+
 post '/lists' do
   list_name = params[:list_name].strip
   error = error_message_finder(list_name)
@@ -76,9 +116,59 @@ post '/lists/:id' do
   end
 end
 
+# Delete a list
 post '/lists/:id/delete' do
   @list_id = params[:id].to_i
   session[:lists].delete_at(@list_id)
   session[:success] = 'List successfully deleted.'
   redirect '/lists'
+end
+
+# Add a new todo to a list
+post '/lists/:id/todos' do
+  @list_id = params[:id].to_i
+  @list = session[:lists][@list_id]
+  text = params[:todo].strip
+
+  error = error_message_finder(text)
+  if error
+    session[:error] = error
+    erb :list
+  else
+    @list[:todos] << {name: text, completed: false}
+      session[:success] = 'The todo has been added.'
+    redirect "/lists/#{@list_id}"
+  end
+end
+
+# Delete a todo from a list
+post '/lists/:id/todos/:todo_id/delete' do
+  @list_id = params[:id].to_i
+  @list = session[:lists][@list_id]
+  @todo_id = params[:todo_id].to_i
+  @list[:todos].delete_at(@todo_id)
+  session[:success] = 'Todo successfully deleted.'
+  redirect "/lists/#{@list_id}"
+end
+
+# Mark todo complete
+post '/lists/:id/todos/:todo_id' do
+  @list_id = params[:id].to_i
+  @list = session[:lists][@list_id]
+  @todo_id = params[:todo_id].to_i
+
+  is_completed = params[:completed] == "true"
+  @list[:todos][@todo_id][:completed] = is_completed
+
+  session[:success] = 'Todo has been updated.'
+  redirect "/lists/#{@list_id}"
+end
+
+post '/lists/:id/complete_all' do
+  @list_id = params[:id].to_i
+  @list = session[:lists][@list_id]
+
+  @list[:todos].each { |todo| todo[:completed] = true }
+  session[:success] = "All todos have been completed."
+  redirect "/lists/#{@list_id}"
 end
